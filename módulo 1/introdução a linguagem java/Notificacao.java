4db41b5c642f953aff42de2ec2bd9620bb1ed004
onunes <onunes@gmail.com>
2024-10-11 19:32:40 -0300

    deletei as subclasses (Notificacao e NotificacaoEmail) - refatorei na classe ExSobrecargaComEscrita

--- módulo 1/introdução a linguagem java/Notificacao.java
+++
@@ -1,9 +0,0 @@
-public class Notificacao {
-    public void enviar(String mensagem){
-        System.out.println("Enviando notificação: " + mensagem);
-    }
-
-    public void enviar(String mensagem, String destinatario){
-        System.out.println("Enviando notificação para " + destinatario + ": " + mensagem);
-    }
-}
