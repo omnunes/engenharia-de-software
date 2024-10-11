73c2fdc1aa182ce1b0c938cc88af5b6861b2c1d0
onunes <onunes@gmail.com>
2024-10-11 19:28:25 -0300

    exemplo de sobrecarga e sobrescrita - refatoracao de classes

--- módulo 1/introdução a linguagem java/ExSobrecargaComSobrescrita.java
+++ módulo 1/introdução a linguagem java/ExSobrecargaComSobrescrita.java
@@ -10,3 +10,27 @@ public class ExSobrecargaComSobrescrita {
     }
 
 }
+
+class Notificacao {
+    public void enviar(String mensagem){
+        System.out.println("Enviando notificação: " + mensagem);
+    }
+
+    public void enviar(String mensagem, String destinatario){
+        System.out.println("Enviando notificação para " + destinatario + ": " + mensagem);
+    }
+}
+
+class NotificacaoEmail extends Notificacao{
+    @Override
+    public void enviar(String mensagem){
+        //Sobrescreve o método enviar para enviar mensagem por email
+        System.out.println("Enviando email: " + mensagem);
+    }
+
+    @Override
+    public void enviar (String mensagem, String destinatario){
+        //Sobrescreve o método enviar para enviar mensagem por email para um destinatário específico
+        System.out.println("Enviando email para " + destinatario + ": " + mensagem);
+    }
+}
